1f2bd7e5ce3969e5667827ff8767fcd7a131014b
Dominic Fiorelli <dominicfiorelli0223@gmail.com>
2022-05-12 09:39:03 -0500

    day3

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const { donnectDb, connectDB } = require('./src/db');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./src/graphql/schema')
+const { authenticate } = require('./src/middleware/auth')
+const cookieParser = require('cookie-parser')
 const path = require('path');
 const app = express();
 const port = 3000
@@ -11,10 +13,16 @@ dotenv.config()
 
 connectDB()
 
+app.use(cookieParser())
+
 app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: true
 }))
+
+app.use(express.urlencoded({ extended: true }))
+
+require("./src/routes")(app);
 
 app.listen(port, ()=>{
     console.log(`hello world app listening at port: ${port}`)
@@ -23,6 +31,10 @@ app.listen(port, ()=>{
 // Setting Templates
 app.set('view engine', 'ejs')
 
+app.set('views', path.join(__dirname, '/src/templates/views'));
+
+app.use(authenticate)
+
 const user = {
     firstName: 'Dominic',
     lastName: 'Fiorelli'
@@ -30,23 +42,23 @@ const user = {
 
 //creating routes
 app.get('/', (req, res) =>{
-    res.render('pages/index', {user:user})
+    res.render('views/index', {user:user})
 })
 
 app.get('/home', (req,res)=>{
-    res.render('pages/home')
+    res.render('views/home')
 })
 
 app.get('/profile', (req,res)=>{
-    res.render("pages/profile")
+    res.render("views/profile")
 })
 
-app.get('/login', (req,res)=>{
-    res.render("pages/login")
-})
+// app.get('/login', (req,res)=>{
+//     res.render("views/login")
+// })
 
 app.get('/register', (req,res)=>{
-    res.render("pages/register")
+    res.render("views/register")
 })
 
 //using middleware
---
+++ src/routes/dashboard/index.js
@@ -0,0 +1,15 @@
+// module.exports = function(app) {
+//     app.use("/auth", require("./auth"))
+//     /*  
+//     app.use("/quiz", require("./quiz"))
+//     app.use("/", require("./dashboard")) 
+//     */
+// };
+
+const MainDashboardRouter = require("express").Router()
+
+
+
+
+
+module.exports = MainDashboardRouter
